20d776206bc502a635f17c7ea30c290a8a6b9e88
dalal <dnajemm@gmail.com>
2025-10-16 05:57:07 +0000

    corrected the input and output directories and added final comments to the code.

--- scripts/variant_calling.sh
+++ scripts/variant_calling.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
-# Script for variant calling using bcftools
+# Script for variant calling using bcftools: mpileup and bcftools: call on BAM files.
+# takes as input the BAM files from the mapping step and a reference genome in fasta format 
+# gives as output compressed VCF files containing the identified variants
 
-REF="/data/projet3/reference/brbr.fasta"            # reference genome
-Mapping_reads_DIR="/data/projet3/results_mapping"   # folder with bam files
-OUT_DIR="/data/projet3/results_variant_calling"     # output folder
-THREADS=8
+REF="/data/projet3/Input/reference/brbr.fasta"            # reference genome
+Mapping_reads_DIR="/data/projet3/Output/results_mapping"   # folder with bam files from mapping step
+OUT_DIR="/data/projet3/Output/results_variant_calling"     # output folder
+THREADS=8                                           # number of CPU cores used
 
 
-for f in $Mapping_reads_DIR/*.bam; do
+for f in $Mapping_reads_DIR/*.bam; do  # loop through all .bam files
     SAMPLE=$(basename "$f" .bam)     # sample name
     echo "Processing $SAMPLE "
     
