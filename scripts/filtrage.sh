380a1d2e49001d0b3acb5be109fd74b9de63c623
dalal <dnajemm@gmail.com>
2025-10-18 14:47:15 +0000

    Correction of the filtering code and addition of this section to the cahier de labo

--- scripts/filtrage.sh
+++ scripts/filtrage.sh
@@ -1,23 +1,22 @@
-#!/usr/bin/env bash
-# Filters VCF files based on per-sample coverage and allele ratio
-VCF_DIR="/data/projet3/Output/results_variant_calling"  # folder with input vcf files
-OUT_DIR="/data/projet3/Output/results_filtering"    # folder for outputs
+#!/bin/bash
 
+VCF_DIR="/data/projet3/Output/results_variant_calling" # Directory containing input VCF files
+OUT_DIR="/data/projet3/Output/results_filtering" # Directory for output filtered VCF files
 
+mkdir -p "$OUT_DIR" #create output directory if it doesn't exist
 
-for vcf in "$VCF_DIR"/*.vcf.gz; do # Loop through each compressed VCF file
-  base=$(basename "${vcf%.vcf.gz}")
-  out="$OUT_DIR/${base}.filtered.vcf.gz"
+for vcf in "$VCF_DIR"/*.vcf.gz; do
+  base=$(basename "${vcf%.vcf.gz}") # Extract base name without extension
+  out="$OUT_DIR/${base}.filtered.vcf.gz" # Define output file path and name
 
-  
-  bcftools view -v snps -m2 -M2 "$vcf" \
-  | bcftools filter -Oz -o "$out" -i '
+  # Filter for heterozygous SNPs with depth >= 5 and allele ratio between 0.4 and 0.6
+  bcftools view -v snps -m2 -M2 "$vcf" \ 
+  | bcftools filter -i '
+      GT="het" &&
       (FORMAT/AD[0:0] + FORMAT/AD[0:1]) >= 5 &&
-      (FORMAT/AD[0:0] + FORMAT/AD[0:1]) > 0 &&
-      (FORMAT/AD[0:1]) / (FORMAT/AD[0:0] + FORMAT/AD[0:1]) >= 0.40 &&
-      (FORMAT/AD[0:1]) / (FORMAT/AD[0:0] + FORMAT/AD[0:1]) <= 0.60
-    '
+      (FORMAT/AD[0:1]) / (FORMAT/AD[0:0] + FORMAT/AD[0:1]) >= 0.4 &&
+      (FORMAT/AD[0:1]) / (FORMAT/AD[0:0] + FORMAT/AD[0:1]) <= 0.6
+    ' -Oz -o "$out"
 
-  bcftools index -f "$out"
+  bcftools index -f "$out" # Index the filtered VCF file
 done
-
